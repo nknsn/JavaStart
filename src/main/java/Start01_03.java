87659541f023f44bff337052b88625ec5fb34497
nkns021 <98322500+nknsn@users.noreply.github.com>
2022-06-25 14:56:54 +0900

    [Finish]1章

--- src/main/java/Start01_03.java
+++ src/main/java/Start01_03.java
@@ -1,4 +1,10 @@
 
 public class Start01_03 {
-
+	public static void main(String[] args) {
+		boolean result = true;
+		char word = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String name = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
 }
